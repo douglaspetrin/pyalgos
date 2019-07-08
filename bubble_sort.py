f8c03f20cf5a1787bedcf30bf5fddc7111857518
Douglas Petrin <editor@douglaspetrin.com>
2019-07-08 16:55:08 -0300

    Bubble Sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+array = [8,7,5,3,1,2,9,6,4]
+
+def sort(array):
+
+    for final in range(len(array), 0, -1):
+        exchanging = False
+
+        for current in range(0, final - 1):
+            if array[current] > array[current + 1]:
+                array[current + 1], array[current] = array[current], array[current + 1]
+                exchanging = True
+
+        if not exchanging:
+            break
+sort(array)
+print(array)
